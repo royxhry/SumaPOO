a0c8a3932809f9eabba7f9e1fa17c69a9cddeb0c
Rodrigo <hl>
2024-05-02 21:09:40 -0600

    version1

--- MainSuma.java
+++ MainSuma.java
@@ -5,17 +5,13 @@ public class MainSuma {
         Scanner p1 = new Scanner(System.in);
         Suma s1 = new Suma();
         System.out.println("ingresa el primer numero ");
-        int a = p1.nextInt();
-        s1.setA(a);   
+        int a = p1.nextInt();   
         System.out.println("ingresa el segundo numero");
         int b = p1.nextInt();
-        s1.setB(b);
-        
-        s1.sumar();
+        int r = s1.Sumar(a, b);
 
-        int r = s1.getR();
         System.out.println("El resultado de la suma es: "+r);
-       System.out.println(s1.toString());
+        p1.close();
        
     }
 
--- Suma.java
+++ Suma.java
@@ -1,34 +1,10 @@
 public class Suma{
     
     
-    private int a,b,r;
-    public int getA() {
-        return a;
-    }
-    
-    public void setA(int a) {
-        this.a = a;
-    }
-    
-    public int getB() {
-        return b;
-    }
-    
-    public void setB(int b) {
-        this.b = b;
-    }
-    
-    public void sumar() {
-        this.r = a+b;
-    }
-    
-    public int getR() {
-        return r;
-    }
-    
-    
-    @Override
-    public String toString() {
-        return "Suma [a=" + a + ", b=" + b + ", r=" + r + "]";
+    public int Sumar(int a, int b){
+        
+        return a+b;
+
+
     }
 }
