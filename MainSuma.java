9696b856ceedf0a75a638527cd8112ceebc3fc19
Rodrigo <hl>
2024-05-02 21:13:43 -0600

    version2

--- MainSuma.java
+++ MainSuma.java
@@ -5,14 +5,18 @@ public class MainSuma {
         Scanner p1 = new Scanner(System.in);
         Suma s1 = new Suma();
         System.out.println("ingresa el primer numero ");
-        int a = p1.nextInt();   
+        int a = p1.nextInt();
+        s1.setA(a);   
         System.out.println("ingresa el segundo numero");
         int b = p1.nextInt();
-        int r = s1.Sumar(a, b);
+        s1.setB(b);
+        
+        s1.sumar();
 
+        int r = s1.getR();
         System.out.println("El resultado de la suma es: "+r);
-        p1.close();
-       
+       System.out.println(s1.toString());
+       p1.close();
     }
 
 }
