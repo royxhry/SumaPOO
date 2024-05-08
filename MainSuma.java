966ac3939fc1ba9c4f37d1026730d07e8de94ca6
Rodrigo <hl>
2024-05-08 00:47:52 -0600

    v3

--- MainSuma.java
+++ MainSuma.java
@@ -2,21 +2,33 @@ import java.util.Scanner;
 
 public class MainSuma {
     public static void main(String[] args) {
+        // Crear un objeto Scanner para leer la entrada del usuario
         Scanner p1 = new Scanner(System.in);
+        
+        // Crear un objeto de tipo Suma
         Suma s1 = new Suma();
-        System.out.println("ingresa el primer numero ");
+        
+        // Solicitar al usuario que ingrese el primer número
+        System.out.println("Ingresa el primer número:");
         int a = p1.nextInt();
-        s1.setA(a);   
-        System.out.println("ingresa el segundo numero");
+        s1.setA(a);
+        
+        // Solicitar al usuario que ingrese el segundo número
+        System.out.println("Ingresa el segundo número:");
         int b = p1.nextInt();
         s1.setB(b);
         
+        // Realizar la suma
         s1.sumar();
 
+        // Obtener el resultado de la suma
         int r = s1.getR();
-        System.out.println("El resultado de la suma es: "+r);
-       System.out.println(s1.toString());
-       p1.close();
+        System.out.println("El resultado de la suma es: " + r);
+        
+        // Imprimir la información de la suma utilizando el método toString
+        System.out.println(s1.toString());
+        
+        // Cerrar el Scanner
+        p1.close();
     }
-
 }
--- Suma.java
+++ Suma.java
@@ -1,32 +1,38 @@
-public class Suma{
+public class Suma {
+    // Variables para los valores y el resultado de la suma
+    private int a, b, r;
     
-    
-    private int a,b,r;
+    // Método para obtener el valor 'a'
     public int getA() {
         return a;
     }
     
+    // Método para establecer el valor 'a'
     public void setA(int a) {
         this.a = a;
     }
     
+    // Método para obtener el valor 'b'
     public int getB() {
         return b;
     }
     
+    // Método para establecer el valor 'b'
     public void setB(int b) {
         this.b = b;
     }
     
+    // Método para realizar la suma de 'a' y 'b'
     public void sumar() {
-        this.r = a+b;
+        this.r = a + b;
     }
     
+    // Método para obtener el resultado de la suma
     public int getR() {
         return r;
     }
     
-    
+    // Método toString para representar la información de la suma
     @Override
     public String toString() {
         return "Suma [a=" + a + ", b=" + b + ", r=" + r + "]";
